8f603dd1c0c7939087fabc230aa0c473069febc6
Katemi Dev <60159484+Katemi-GitHub@users.noreply.github.com>
2022-03-10 23:59:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ screen = pygame.display.set_mode((600, 600))
 display = pygame.Surface((128, 128))
 clock = pygame.time.Clock()
 
-pop = 20
+pop = 200
 cells = []
 steps = 100
 steps_u = 0
+mut = 0.1
 for i in range(pop):
     cells.append(Cell(random.randint(0, 128), random.randint(0, 128), None, 10))
 temp_cells = []
@@ -31,15 +32,28 @@ while True:
             cell.update(display, 'black', 128, 128)
     else:
         for cell in cells:
-            if cell.x < 100:
+            if cell.x < 64:
                 temp_cells.append(Cell(random.randint(0, 128), random.randint(0, 128), cell.brain.genome, 0))
-        
+                temp_cells.append(Cell(random.randint(0, 128), random.randint(0, 128), cell.brain.genome, 0))
+    
         cells.clear()
         cells = temp_cells
         temp_cells = []
         steps_u = 0
     steps_u += 1
 
+    if len(cells) > pop:
+        del_c = len(cells) - pop
+        for i in range(del_c):
+            cells.pop(0)
+    
+    if mut == 1:
+        mut = 0
+        for cell in cells:
+            cell.mutation()
+    
+    mut += mut
+
     screen.blit(pygame.transform.scale(display, (600, 600)), (0, 0))
     pygame.display.update()
-    clock.tick(10)
+    clock.tick(60)
